e3cf50c2f03b7ad19890d3e61de4abf09a3ae6d5
Mert5 <39389585+Mert5@users.noreply.github.com>
2024-09-30 16:57:16 +0300

    Add files via upload

---
+++ 13.6.PassByReference/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+// REFERENCE => freeCodeCamp.org
+
+void say_age(int &age);
+
+int main(){
+
+    int age {35};
+    
+    std::cout << "Your age, before function call is : " << age << ", and the address is : " << &age << std::endl;
+    say_age(age);
+    std::cout << "Your age, after function call is : " << age << ", and the address is : " << &age << std::endl;
+
+    return 0;
+}
+
+void say_age(int &age){
+    age++;      // in function body, you do not have to make dereferencing
+    std::cout << "Your age in function is : " << age << ", and the address is : " << &age << std::endl;   
+}
